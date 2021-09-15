2eb782131e7ff52aa8859c4de278cf94f6f2ffe4
Trace <tracedelange@me.com>
2021-09-15 10:58:40 -0500

    Added more styling to POI-details, also restructured some styling and added overflow scrolling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 
-import {BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import PointOfInterestDetails from './components/PointOfInterestDetails';
 import PointOfInterestForm from './components/PointOfInterestForm';
 
 
-const  App= ()=> {
+const App = () => {
   return (
 
-     <Router>
-       <Navbar/>
-       <div className="ml-20">
-       <Switch>
-         <Route path='/pointsofinterests/:id'>
-            <PointOfInterestDetails />
-         </Route>
-         <Route exact path='/'>
-           <Home/>
-         </Route>
-         <Route exact path='/test'>
-          <h1>Travelers' Tricks!</h1>
-         </Route>
-         <Route exact path='/new'>
-          <PointOfInterestForm />
-         </Route>
-       </Switch>
+    <Router>
+
+      <div className='flex flex-row overscroll-auto'>
+        <Navbar />
+        <div className="h-screen w-screen overflow-y-scroll overscroll-y-contain bg-blue-50">
+          <Switch>
+            <Route path='/pointsofinterests/:id'>
+              <PointOfInterestDetails />
+            </Route>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/test'>
+              <h1>Travelers' Tricks!</h1>
+            </Route>
+            <Route exact path='/new'>
+              <PointOfInterestForm />
+            </Route>
+          </Switch>
         </div>
-     </Router>
+
+      </div>
+    </Router>
 
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,49 +4,53 @@ import icons from '../data'
 const Navbar = () => {
   return (
     <>
-    <div className='absolute top-0 left-0 bottom-0
+
+      {/* absolute top-0 left-0 bottom-0 */}
+      {/* Changed this to flex-box positioning */}
+
+      <div className='
          bg-gradient-to-t from-blue-500 
          via-purple-500 to-purple-700 
-         flex flex-col '>
-           <div className='flex flex-col'>
-               <NavLink
-           to='/'
-           >
-          <i>
-          {icons[0].svg}
-         </i>
-             </NavLink>
-             <NavLink
-           to='/review'
-           >  
-           <i>
-            {icons[3].svg}
-         </i>
-             </NavLink>
-             <NavLink to='/'>
+         flex flex-col'>
+        <div className='flex flex-col'>
+          <NavLink
+            to='/'
+          >
+            <i>
+              {icons[0].svg}
+            </i>
+          </NavLink>
+          <NavLink
+            to='/review'
+          >
+            <i>
+              {icons[3].svg}
+            </i>
+          </NavLink>
+          <NavLink to='/'>
             <i>
               {icons[1].svg}
-         </i>
-            </NavLink>
-            <NavLink to='/'>
+            </i>
+          </NavLink>
+          <NavLink to='/'>
             <i>{icons[2].svg}</i>
-            </NavLink>
-            <NavLink to='/new'>
-           <i> {icons[4].svg} </i>
-            </NavLink>
-           </div>
-           <div>
-               <NavLink to='/login'>
-           <i> {icons[5].svg} </i>
-            </NavLink>
+          </NavLink>
+          <NavLink to='/new'>
+            <i> {icons[4].svg} </i>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to='/login'>
+            <i> {icons[5].svg} </i>
+          </NavLink>
 
-           </div>
-          
-   </div>
-    
- 
+        </div>
+
+      </div>
+
+
     </>
-    
+
   )
 }
 
