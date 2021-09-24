0775930d1cae66cfc4a71c692affe45a99e7cc11
AZIZXlaouiti <azizrocke@gmail.com>
2021-09-24 16:06:18 -0400

    fixed landing page width issue

--- src/components/POI/PointOfInterestCard.js
+++ src/components/POI/PointOfInterestCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import icons from '../../assets/icons/data'
 const PointOfInterestCard = ({ poi }) => {
 
-  // console.log(poi)
+  console.log("poi",poi)
   let Background = poi.image
   return (
 
